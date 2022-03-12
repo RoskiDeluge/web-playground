73e561bf92d6b4b37505c3523ec83bea7a59ffe0
RoskiDeluge <roberto.delgado@gmail.com>
2022-03-11 15:57:16 -1000

    innerHTML working

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-import './style.css';
-
 document.querySelector('#app').innerHTML = `
-  <h1>DOM Magic</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h2>DOM Magic</h2>
+  <p><a href="https://vitejs.dev/guide/features.html" target="_blank">Vite.js Documentation</a></p>
 `;
